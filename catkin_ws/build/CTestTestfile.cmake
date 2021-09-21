712779ab33db409356946c4286c9ef78de71248b
Tharaka Wijesundara <wmtharaka98@gmail.com>
2021-09-21 08:22:03 +0000

    Added raw urdf files

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/tharaka/ROS/fyp/catkin_ws/src
+# Build directory: /home/tharaka/ROS/fyp/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("fyp_sim")

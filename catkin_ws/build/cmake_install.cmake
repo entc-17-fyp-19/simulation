eb0aeb0ebe3c23fdd9655042e56e770cc99ae026
Tharaka Wijesundara <wmtharaka98@gmail.com>
2021-10-31 04:16:35 +0000

    Added controllers, fixed URDF, added GPS and tested

--- catkin_ws/build/cmake_install.cmake
+++ catkin_ws/build/cmake_install.cmake
@@ -132,6 +132,7 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/tharaka/ROS/fyp/catkin_ws/build/gtest/cmake_install.cmake")
   include("/home/tharaka/ROS/fyp/catkin_ws/build/fyp_sim/cmake_install.cmake")
+  include("/home/tharaka/ROS/fyp/catkin_ws/build/platform_controller/cmake_install.cmake")
 
 endif()
 
